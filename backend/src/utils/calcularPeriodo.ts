2bf56af75ee7733fe944aac795d32fdd75ba9e07
IsaqueTADS <isaqueofc18@gmail.com>
2026-01-08 20:37:42 -0300

    função para calcular periodo

---
+++ backend/src/utils/calcularPeriodo.ts
@@ -0,0 +1,31 @@
+export default function calcularPeriodo(periodo?: string) {
+  if (!periodo) return undefined;
+
+  const agora = new Date();
+
+  if (periodo === "ultimos_3_meses") {
+    const inicio = new Date();
+    inicio.setMonth(agora.getMonth() - 3);
+    return { gte: inicio };
+  }
+
+  if (periodo === "ultimos_2_meses") {
+    const inicio = new Date();
+    inicio.setMonth(agora.getMonth() - 2);
+    return { gte: inicio };
+  }
+
+  if (periodo === "ultimo_mes") {
+    const inicio = new Date();
+    inicio.setMonth(agora.getMonth() - 1);
+    return { gte: inicio };
+  }
+
+  if (periodo === "ultima_semana") {
+    const inicio = new Date();
+    inicio.setDate(agora.getDate() - 7);
+    return { gte: inicio };
+  }
+
+  return undefined;
+}
