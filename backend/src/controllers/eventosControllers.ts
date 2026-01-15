27231f60208e06bbbc1d08ba5de94e64ac538c23
IsaqueTADS <isaqueofc18@gmail.com>
2026-01-14 23:29:51 -0300

    a rota de buscar evento unico agora retorna id da cidade e do estado

--- backend/src/controllers/eventosControllers.ts
+++ backend/src/controllers/eventosControllers.ts
@@ -302,8 +302,9 @@ export async function buscarEvento(req: Request, res: Response) {
       include: {
         cidade: {
           select: {
+            id: true,
             nome: true,
-            estado: { select: { uf: true, nome: true } },
+            estado: { select: { id: true, uf: true, nome: true } },
           },
         },
         usuario: {
