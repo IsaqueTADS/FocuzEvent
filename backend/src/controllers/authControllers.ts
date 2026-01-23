200afa7846a69721bcb872e3ffe0c8d59a43efba
IsaqueTADS <isaqueofc18@gmail.com>
2026-01-23 18:15:08 -0300

    rota para redifinir senha

--- backend/src/controllers/authControllers.ts
+++ backend/src/controllers/authControllers.ts
@@ -2,6 +2,8 @@ import bcrypt from "bcrypt";
 import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { env } from "src/env/index.js";
+import enviarEmailRecuperacao from "src/utils/enviarEmailRecuperacao.js";
+import gerarCodigoRecuperacao from "src/utils/gerarCodigoRecuperacao.js";
 import { z } from "zod";
 
 import prisma from "../utils/prisma.js";
@@ -96,3 +98,56 @@ export async function logarUsuario(req: Request, res: Response) {
 export async function verificarToken(req: Request, res: Response) {
   res.status(200).json({ ok: true });
 }
+
+export async function recuperarSenha(req: Request, res: Response) {
+  try {
+    const authSchema = z.object({
+      email: z.email(),
+    });
+
+    const validacao = authSchema.safeParse(req.body);
+
+    if (validacao.success === false) {
+      res.status(400).json({ error: "Digite um email válido." });
+      return;
+    }
+
+    const { email } = validacao.data;
+
+    const usuario = await prisma.usuario.findUnique({
+      where: { email },
+    });
+
+    if (!usuario) {
+      res.status(200).json({
+        mensagem: "Código de recuperação enviado para o email. 2",
+      });
+
+      return;
+    }
+
+    const codigo = gerarCodigoRecuperacao();
+    const codigoHash = await bcrypt.hash(codigo, 10);
+
+    await prisma.usuario.update({
+      where: { id: usuario.id },
+      data: {
+        codigoRecuperacaoHash: codigoHash,
+        codigoRecuperacaoExpira: new Date(Date.now() + 15 * 60 * 1000),
+      },
+    });
+
+    console.log("testando");
+    await enviarEmailRecuperacao(usuario.email, codigo);
+
+    res.json({
+      mensagem: "Código de recuperação enviado para o email.",
+    });
+    return;
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Erro interno ao tentar logar usuarios" });
+  }
+}
+
+console.log("chamou");
--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -3,6 +3,7 @@ import express from "express";
 import {
   criarUsuario,
   logarUsuario,
+  recuperarSenha,
   verificarToken,
 } from "../controllers/authControllers";
 import autenticacao from "../middlewares/authMiddleware";
@@ -11,6 +12,7 @@ const authRouter = express.Router();
 
 authRouter.post("/register", criarUsuario);
 authRouter.post("/login", logarUsuario);
+authRouter.post("/recuperar-senha", recuperarSenha);
 authRouter.get("/token/validar", autenticacao, verificarToken);
 
 export default authRouter;
