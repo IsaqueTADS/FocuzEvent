e9d0398eaa6a50b7d4cb2cc4f91acfc87f87519b
IsaqueTADS <isaqueofc18@gmail.com>
2026-01-23 18:14:20 -0300

    nova mensagem de recuperacao de senha

--- backend/src/utils/enviarEmailRecuperacao.ts
+++ backend/src/utils/enviarEmailRecuperacao.ts
@@ -1,6 +1,11 @@
 import nodemailer from "nodemailer";
 import { env } from "src/env";
 
+console.log({
+  user: env.EMAIL_USUARIO,
+  pass: env.EMAIL_SENHA_APP,
+});
+
 const transportadorEmail = nodemailer.createTransport({
   service: "gmail",
   auth: {
@@ -14,13 +19,37 @@ export default async function enviarEmailRecuperacao(
   codigo: string,
 ) {
   await transportadorEmail.sendMail({
-    from: "Suporte <focuz@event.com>",
+    from: "Suporte Focuz <focuz@event.com>",
     to: email,
     subject: "Recuperação de senha",
     html: `
-      <p>Use o código abaixo para redefinir sua senha:</p>
-      <h2>${codigo}</h2>
-      <p>Este código expira em 15 minutos.</p>
-    `,
+    <p>Olá,</p>
+
+    <p>Recebemos uma solicitação para redefinir a senha da sua conta.</p>
+
+    <p>Para continuar, utilize o código de verificação abaixo:</p>
+
+    <h2 style="letter-spacing: 2px;">${codigo}</h2>
+
+    <p>
+      Este código é válido por <strong>15 minutos</strong>.
+      Após esse período, será necessário solicitar uma nova recuperação de senha.
+    </p>
+
+    <p>
+      <strong>Atenção:</strong><br />
+      Caso você <strong>não tenha solicitado</strong> a redefinição de senha,
+      recomendamos que altere sua senha imediatamente e entre em contato com nosso suporte. focuz@event.com
+    </p>
+
+    <p>
+      Este é um e-mail automático. Por favor, <strong>não responda</strong> a esta mensagem.
+    </p>
+
+    <p>
+      Atenciosamente,<br />
+      <strong>Equipe Focuz</strong>
+    </p>
+  `,
   });
 }
